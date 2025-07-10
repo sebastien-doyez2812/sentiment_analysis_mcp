b75f1004abc4e7c462804ca28310ba0f2ea2abd7
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-07-09 22:07:02 -0400

    server for sentiment_analysis, using textblob

---
+++ app.py
@@ -0,0 +1,34 @@
+import json
+import gradio as gr
+from textblob import TextBlob
+
+
+
+def sentiment_analysis(text: str) -> str:
+    """
+    Args:
+        text, a string, it's the text you want to make a sentiment analysis on
+    Return a Json String, 
+        - polarity
+        - subjectivity
+        - assessment
+    """
+    blob = TextBlob(text)
+    data = {
+        "polarity": round(blob.sentiment.polarity, 2),
+        "subjectivity" : round(blob.sentiment.subjectivity, 2),
+         "assessment": "positive" if blob.sentiment.polarity > 0 else "negative" if blob.sentiment.polarity < 0 else "neutral"
+    }
+    return json.dumps(data)
+
+
+
+demo = gr.Interface(fn = sentiment_analysis,
+                    inputs= gr.Textbox(placeholder= "Enter your text, and our system will give you the sentiment"),
+                    outputs= gr.Textbox(),
+                    title= "Sentiment Analysis")
+
+
+
+if __name__ == "__main__":
+    demo.launch(mcp_server=True)
